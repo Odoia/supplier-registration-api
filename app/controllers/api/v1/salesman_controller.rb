d47f141575466190852d1c205f61d964732eb562
tiago <tiagobrunobfr@gmail.com>
2020-07-06 11:55:19 -0300

    update a salesman with problem in route

--- app/controllers/api/v1/salesman_controller.rb
+++ app/controllers/api/v1/salesman_controller.rb
@@ -15,6 +15,19 @@ module Api
           render status: 201, json: { data: salesman, status: 201 }
         end
       end
+
+      def update
+        salesman = ::Salesman.find(params[:id])
+        if salesman.valid?
+          salesman.name = params[:name]
+          salesman.status = params[:status]
+          salesman.save
+          render status: 200, json: {data: salesman, status:200}
+        else
+          render status: 404, json: {status: 404, data:'Not Found'}
+        end
+      end
+
     end
   end
 end
